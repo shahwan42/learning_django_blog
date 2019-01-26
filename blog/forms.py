01c55bdebd9a8b0cd7229be0545bb637d7ebaa4c
Ahmed Loay <ahmed@shahwan.me>
2019-01-27 01:11:05 +0200

    Commenting System
    - Comment Model
    - Comment(ModelForm)
    - Comment Model Migration
    - Model Admin Registration
    - Post Detail view function to handle comments
    and post_detail.html to show comments available
    or message. and the form to post a new comment

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from .models import Comment
 
 
-class EmailPostForm(forms.Form):
+class EmailPostForm(forms.Form):  # a standalone form
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):  # form from a model
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -37,9 +37,29 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # list of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # a comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 
 def post_share(request, post_id):
