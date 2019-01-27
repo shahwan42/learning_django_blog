c9fd035726c8f9c529e397d6f495be27b0ebffc4
Ahmed Loay <ahmed@shahwan.me>
2019-01-27 02:13:26 +0200

    Tags to Posts and filtering posts with a certain tag.
    Revert to post_list function instead of the ListView

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -31,6 +32,8 @@ class Post(models.Model):
 
     objects = models.Manager()  # default model manager
     published = PublishedManager()  # our custom manager
+
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
@@ -14,21 +15,26 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#     object_list = Post.objects.all()
-#     paginator = Paginator(object_list, 3)  # 3 posts in each page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,
-#                   'blog/post/list.html',
-#                   {'page': page, 'posts': posts})
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
